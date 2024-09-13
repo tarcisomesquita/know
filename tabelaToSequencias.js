15153122211ec74e8eecf35adb9aa9ae4157348e
Tarciso Mesquita <tarcisomesquita@gmail.com>
2024-09-13 11:14:24 -0300

    Update tabelaToSequencias.js

--- tabelaToSequencias.js
+++ tabelaToSequencias.js
@@ -29,6 +29,28 @@ async function htmlToJson(html) {
   return json;
 }
 
+async function htmlToJsonArray(html) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+  const table = doc.querySelector('table');
+  const linhas = table.querySelectorAll('tr');
+  const cells = linhas[0].querySelectorAll('th');
+  
+  const header = [];
+  for (let i = 0; i < cells.length; i++) header.push(cells[i].textContent);
+  
+  const jsonArray = [];
+
+  for (let i = 1; i < linhas.length; i++) {
+    const cells = linhas[i].querySelectorAll('td');
+    const jsonObject = {};
+    for (let j = 0; j < header.length; j++) jsonObject[header[j]] = cells[j].textContent;
+    jsonArray.push(jsonObject);
+  }
+
+  return jsonArray;
+}
+
 (async () => {
   const palavra = await getContent('palavra');
   if (! palavra) {
